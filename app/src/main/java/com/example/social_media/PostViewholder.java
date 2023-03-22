f18d6fa627c52d0bfc1ee6fe289b93538c24b3de
san_m <ajayiyer30@gmail.com>
2023-03-22 20:23:21 +0530

    Created Uploading and retriving the data

---
+++ app/src/main/java/com/example/social_media/PostViewholder.java
@@ -0,0 +1,90 @@
+package com.example.social_media;
+
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class PostViewholder extends RecyclerView.ViewHolder {
+
+    ImageView imageViewprofile,iv_post;
+    TextView tv_name, tv_desc,tv_likes,tv_comment,tv_time,tv_nameprofile;
+    ImageButton likebtn,menuoptions,commentbtn;
+    DatabaseReference likesref;
+    FirebaseDatabase database =FirebaseDatabase.getInstance();
+    int likescount;
+    public PostViewholder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void SetPost(FragmentActivity activity,String name ,String url,String postUri,String time,String uid,String type,String desc){
+        imageViewprofile =itemView.findViewById(R.id.iv_profile_item);
+        iv_post=itemView.findViewById(R.id.iv_post_item);
+        tv_comment=itemView.findViewById(R.id.commentbutton_post);
+        tv_desc = itemView.findViewById(R.id.tv_desc_post);
+        commentbtn=itemView.findViewById(R.id.commentbutton_post);
+        likebtn=itemView.findViewById(R.id.likebutton_post);
+        tv_likes=itemView.findViewById(R.id.tv_likes_post);
+        menuoptions=itemView.findViewById(R.id.morebutton_post);
+        tv_time=itemView.findViewById(R.id.tv_time_post);
+        tv_nameprofile=itemView.findViewById(R.id.tv_name_post);
+
+        SimpleExoPlayer exoPlayer;
+        PlayerView playerView =itemView.findViewById(R.id.exoplayer_item_post);
+        if(type.equals("iv")){
+            Picasso.get().load(url).into(imageViewprofile);
+            Picasso.get().load(postUri).into(iv_post);
+            tv_desc.setText(desc);
+            tv_time.setText(time);
+            tv_nameprofile.setText(name);
+            playerView.setVisibility(View.INVISIBLE);
+        } else if (type.equals("vv")) {
+            iv_post.setVisibility(View.INVISIBLE);
+            tv_desc.setText(desc);
+            tv_time.setText(time);
+            tv_nameprofile.setText(name);
+            Picasso.get().load(url).into(imageViewprofile);
+            try{
+                BandwidthMeter bandwidthMeter=new DefaultBandwidthMeter.Builder(activity).build();
+                TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+                exoPlayer =(SimpleExoPlayer) ExoPlayerFactory.newSimpleInstance(activity);
+                Uri video =Uri.parse(postUri);
+                DefaultHttpDataSourceFactory df =new DefaultHttpDataSourceFactory("video");
+                ExtractorsFactory ef = new DefaultExtractorsFactory();
+                MediaSource mediaSource =new ExtractorMediaSource(video,df,ef,null,null);
+                playerView.setPlayer(exoPlayer);
+                exoPlayer.prepare(mediaSource);
+                exoPlayer.setPlayWhenReady(false);
+
+
+            }catch (Exception e){
+                Toast.makeText(activity, "Error", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+}
